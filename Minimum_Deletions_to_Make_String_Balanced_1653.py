100630953bc9b05dc532edd9b4f6b3241e75b86b
Rahul Nallapaneni <116174591+lordmavles@users.noreply.github.com>
2026-02-10 07:59:43 +0530

    Delete Minimum_Deletions_to_Make_String_Balanced_1653.py

--- Minimum_Deletions_to_Make_String_Balanced_1653.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minimumDeletions(self, s: str) -> int:
-        nextA, nextB = 0, 0
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == 'a':
-                nextB += 1
-            else:
-                nextA = min(nextA + 1, nextB)
-        return min(nextA, nextB)
